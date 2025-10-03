2cc5357145001f799dbf41759c7e64ac012604df
TinyRender <3287936951@qq.com>
2025-10-03 21:29:08 +0800

    显示灰度图

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,10 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255,   0,   0, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
-std::tuple<int,int> project(vec3 v) { // First of all, (x,y) is an orthogonal projection of the vector (x,y,z).
-    return { (v.x + 1.) *  width/2,   // Second, since the input models are scaled to have fit in the [-1,1]^3 world coordinates,
-             (v.y + 1.) * height/2 }; // we want to shift the vector (x,y) and then scale it to span the entire screen.
+std::tuple<int,int,int> project(vec3 v) { // First of all, (x,y) is an orthogonal projection of the vector (x,y,z).
+    return { (v.x + 1.) *  width/2,       // Second, since the input models are scaled to have fit in the [-1,1]^3 world coordinates,
+             (v.y + 1.) * height/2,       // we want to shift the vector (x,y) and then scale it to span the entire screen.
+             (v.z + 1.) *   255./2 };
 }
 
 void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
@@ -46,7 +47,7 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
 double sign(int ax,int ay,int bx,int by,int cx,int cy){
     return .5*((bx-ax)*(cy-by)-(cx-bx)*(by-ay));
 }
-void TriangleRasterization(int ax,int ay,int bx,int by,int cx,int cy,TGAImage &framebuffer,TGAColor color){
+void TriangleRasterization(int ax,int ay,int az,int bx,int by,int bz,int cx,int cy,int cz,TGAImage &framebuffer,TGAImage &zbuffer,TGAColor color){
     //line(ax,ay,bx,by,framebuffer,red);
     //line(ax,ay,cx,cy,framebuffer,green);
     //line(bx,by,cx,cy,framebuffer,blue);
@@ -55,7 +56,7 @@ void TriangleRasterization(int ax,int ay,int bx,int by,int cx,int cy,TGAImage &f
     int ymin=min(ay,min(by,cy));
     int ymax=max(ay,max(by,cy));
     double signA=sign(ax,ay, bx, by, cx, cy);
-    cout<<signA<<endl;
+    //cout<<signA<<endl;
     if(signA<0) return;
 
     for(int i=xmin;i<=xmax;i++){
@@ -64,13 +65,8 @@ void TriangleRasterization(int ax,int ay,int bx,int by,int cx,int cy,TGAImage &f
              double beta=sign(i,j,cx,cy,ax,ay)/signA;
              double gamma=sign(i,j,ax,ay,bx,by)/signA;
             if(alpha<0||beta<0||gamma<0) continue;//任意一个小于0，代表此像素点在三角形外，不去渲染.
-            //红绿蓝的渐变色三角形填充
-            // unsigned char b=static_cast<unsigned char>(alpha*blue[0]+beta*green[0]+gamma*red[0]);
-            // unsigned char g=static_cast<unsigned char>(alpha*blue[1]+beta*green[1]+gamma*red[1]);
-            // unsigned char r=static_cast<unsigned char>(alpha*blue[2]+beta*green[2]+gamma*red[2]);    
-            
-            // framebuffer.set(i,j,{b,g,r,255});
-
+            unsigned char z=static_cast<unsigned char>(alpha*az+beta*bz+gamma*cz);
+            zbuffer.set(i,j,{z});
             framebuffer.set(i,j,color);
         }
     }
@@ -85,18 +81,18 @@ int main(int argc, char** argv) {
     }
     Model model(argv[1]);
     TGAImage framebuffer(width, height, TGAImage::RGB);
+    TGAImage zbuffer(width,height,TGAImage::GRAYSCALE);
     
-    // int ax=0,ay=0,cx=50,cy=280,bx=260,by=70;
-    // TriangleRasterization(ax,ay,bx,by,cx,cy,framebuffer);
     for (int i=0; i<model.nfaces(); i++) { // iterate through all triangles
-        auto [ax, ay] = project(model.vert(i, 0));
-        auto [bx, by] = project(model.vert(i, 1));
-        auto [cx, cy] = project(model.vert(i, 2));
+        auto [ax, ay, az] = project(model.vert(i, 0));
+        auto [bx, by, bz] = project(model.vert(i, 1));
+        auto [cx, cy, cz] = project(model.vert(i, 2));
         TGAColor rnd;
         for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
-        TriangleRasterization(ax, ay, bx, by, cx, cy, framebuffer, rnd);
+        TriangleRasterization(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer,zbuffer, rnd);
     }
 
-    framebuffer.write_tga_file("framebuffer1.tga");
+    framebuffer.write_tga_file("framebuffer.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
     return 0;
 }
